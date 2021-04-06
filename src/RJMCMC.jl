97cb81a44e7fb17015edd9e79cb5b42a0c122dd8
Keith Lidke <klidke@unm.edu>
2021-04-05 22:51:49 -0600

    Skeleton and MCMC only working

--- src/RJMCMC.jl
+++ src/RJMCMC.jl
@@ -1,110 +1,110 @@
 module RJMCMC
 
+include("BAMF.jl")
+using .BAMF
+
 burnlength=1000;
 chainlength=1000;
 
-mutable struct rjstruct
+mutable struct RJMCMCStruct
     burnin::Int32
     iterations::Int32
     njumptypes::Int32 #number of jump types for RJMCMC
     jumpprobability::Vector{Float32} #sums to 1
-
+    proposalfuns
+    acceptfuns
 end
 
-mutable struct rjstate
-    n::Int32
-    x::Vector{Float32}
-    y::Vector{Float32}
-    photons::Vector{Float32}
-end
-rjstate()=rjstate(1,[0],[0],[0]) #outer constructor
-
-mutable struct rjchain  #this is the main output of RJMCMC
+mutable struct RJChain  #this is the main output of RJMCMC
     n::Int32  #number of jumps in chain 
-    states::Vector{rjstate}
+    states::Vector{Any}
     jumptypes::Vector{Int32}
-    alpha::Vector{Float32}
+    α::Vector{Float32}
     accept::Vector{Bool}
 end
-rjchain(n::Int32)=rjchain(n,
-Vector{rjstate}(undef,n),
+RJChain(n::Int32)=RJChain(n,
+Vector{Any}(undef,n),
 Vector{Int32}(undef,n),
 Vector{Float32}(undef,n),
 Vector{Bool}(undef,n),
 )
-
-function initstate(rjs::rjstruct) #use priors to get initial state
-    n=3 #get this from prior on number
-    x=zeros(Float32,n)
-    y=zeros(Float32,n)
-    photons=zeros(Float32,n)
-    for ii=1:n
-        x[ii]=0
-        y[ii]=0
-        photons[ii]=100
-    end
-    return rjstate(n,x,y,photons)
+function length(rjc::RJChain)
+    return rjc.n
 end
 
 
-function initchain(rjs::rjstruct,burninchain::rjchain) #this gets last state of burnin chain and intializes new chain
-    newchain=rjchain(rjs.iterations)
+function initchain(rjs::RJMCMCStruct,burninchain::RJChain) #this gets last state of burnin chain and intializes new chain
+    newchain=RJChain(rjs.iterations)
     newchain.states[1]=burninchain.states[burninchain.n]
     newchain.accept[1]=1
-    newchain.alpha[1]=1
+    newchain.α[1]=1
     newchain.jumptypes[1]=0
 return newchain
 end
 
-function initchain(rjs::rjstruct) #this initializes new chain from priors and uses burnin length
-    newchain=rjchain(rjs.burnin)
-    newchain.states[1]=initstate(rjs)
+function initchain(rjs::RJMCMCStruct,intialstate) #this initializes new chain given an intial state and configures a burnin
+    newchain=RJChain(rjs.burnin)
+    newchain.states[1]=intialstate
     newchain.accept[1]=1
-    newchain.alpha[1]=1
+    newchain.α[1]=1
     newchain.jumptypes[1]=0
 return newchain
 end
 
-function jtrand(cs::rjstruct) #select a jump type
+function jtrand(rjs::RJMCMCStruct) #select a jump type
     r=rand(Float32);
     jt=1
-    tmp=cs.jumpprobability[jt]
+    tmp=rjs.jumpprobability[jt]
     while r>tmp
         jt+=1
-        if jt==cs.njumptypes
+        if jt==rjs.njumptypes
             break
         end
-        tmp=cs.jumpprobability[jt]
+        tmp=rjs.jumpprobability[jt]
     end    
     return jt
 end
 
-function buildchain(rjs::rjstruct)
+function buildchain(rjs::RJMCMCStruct,mhs,intialstate)
     
     #init and burnin
-    bchain=initchain(rjs)
-    runchain!(rjs,bchain,rjs.burnin)
+    bchain=initchain(rjs,intialstate)
+    runchain!(rjs,bchain,rjs.burnin,mhs)
 
     #real chain
     chain=initchain(rjs,bchain)
-    runchain!(rjs,chain,rjs.iterations)
+    runchain!(rjs,chain,rjs.iterations,mhs)
 
     return chain
 end
 
 
-function runchain!(rjs::rjstruct,rjc::rjchain,iterations)
-    for nn=1:iterations
+function runchain!(rjs::RJMCMCStruct,rjc::RJChain,iterations,mhs)
+    for nn=1:iterations-1
         jt=jtrand(rjs)
-
-        rjc.accept[nn]=1;
         rjc.jumptypes[nn]=jt;
-        rjc.alpha[nn]=1;
-        rjc.states[nn]=rjstate();
 
-        println((nn,jt))
+        #get proposal
+        mtest=rjs.proposalfuns[jt](mhs,rjc.states[nn])     
+
+        #calculate acceptance probability
+        α=rjs.acceptfuns[jt](mhs,rjc.states[nn],mtest)
+        rjc.α[nn]=α;
+
+        #update chain
+        if α>rand()
+            rjc.accept[nn]=1;
+            rjc.states[nn+1]=mtest;
+        else
+            rjc.accept[nn]=0;
+            rjc.states[nn+1]=rjc.states[nn];
+        end
+       
+        #println((nn,jt,α))
     end
 end
+
+
 
 
 end
--- src/RunBAMF.jl
+++ src/RunBAMF.jl
@@ -1,20 +1,72 @@
 #Demonstrates use of BAMF on a single region
+using Plots
+#using ImageView
 
+## 
 include("RJMCMC.jl")
 include("BAMF.jl")
-
 using .RJMCMC
 
-#setup the RJMCMC model
-njumptypes=3
-jumpprobability=[1,2,3]
+#Jumptypes are:
+#move, bg, add,remove, split,merge
+
+
+
+## Create a dataset 
+
+# creating a state
+n=1
+sz=Int32(16)
+sigma=1.3f0
+x=[sz/2]
+y=[sz/2]
+photons=[777]
+bg=1f-4
+datastate=BAMF.StateFlatBg(n,x,y,photons,bg)
+# create a model
+noisyroi=BAMF.ArrayDD(sz)
+BAMF.genmodel_2Dgauss!(datastate,sz,sigma,noisyroi.data)
+
+
+## Create a BAMF RJ struct
+xystd=sigma/10;
+istd=10;
+myRJ=BAMF.RJStructDD(sz,sigma,xystd,istd,noisyroi)
+
+## Setup the RJMCMC.jl model
+
+njumptypes=6
+jumpprobability=[1,0,0,0,0,0] #Move only
 jumpprobability=jumpprobability/sum(jumpprobability)
 
-#create a structure with all model info
-iterations=10
-burnin=20
-myrjs=RJMCMC.rjstruct(burnin,iterations,njumptypes,jumpprobability)
+# create a structure with all model info
+iterations=100
+burnin=1
+acceptfuns=[BAMF.accept_move]
+propfuns=[BAMF.propose_move]
+myRJMCMC=RJMCMC.RJMCMCStruct(burnin,iterations,njumptypes,jumpprobability,propfuns,acceptfuns)
 
-mychain=RJMCMC.buildchain(myrjs)
+#create an intial state
+state1=BAMF.calcintialstate(myRJ)
+
+#run chain
+#ImageView.closeall()
+mychain=RJMCMC.buildchain(myRJMCMC,myRJ,state1)
+
+## Display
+
+
+
+jts=mychain.jumptypes
+histogram(jts)
+
+histogram(mychain.α)
+histogram(mychain.accept)
+
+
+zoom=Int32(20);
+BAMF.histogram2D(mychain.states,sz,zoom)
+
+
 
 
