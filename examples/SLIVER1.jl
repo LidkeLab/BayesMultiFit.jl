65d5dfd1ded5cf02cb3f50402011578c9ffcb4ca
StapleGrain <StapleGrain@github.com>
2021-07-30 08:42:00 -0400

    added note that this is an example of new meastype

--- examples/SLIVER1.jl
+++ examples/SLIVER1.jl
@@ -5,8 +5,9 @@ BAMF = BayesMultiFit
 SLIVER1DXMeasType(images::Int32, inttime::Float32, invx::Float32, invy::Float32)
 
 Contains the information necessary to create a SLIVER measurement, the integration time, inttime, the xy coordinates of the inversion point, invx and invy, and the static number of images a SLIVER measurement creates, images.
-"""
 
+THIS IS AN EXAMPLE OF AN ADAPTIVE MEASUREMENT TYPE AS IT WOULD BE ADDED TO THE AdaptiveMeasurements PACKAGE
+"""
 struct SLIVER1DXMeasType <: BAMF.MeasType
     images::Int32
     inttime::Float32
